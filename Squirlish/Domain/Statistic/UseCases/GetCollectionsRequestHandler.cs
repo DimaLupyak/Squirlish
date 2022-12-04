2f4da4dfb485c07de2c221957054708346388a5a
DmytroL <Dmytro.Lupiak@nice.com>
2022-12-05 01:53:45 +0200

    Addded real statistic

--- Squirlish/Domain/Statistic/UseCases/GetCollectionsRequestHandler.cs
+++ Squirlish/Domain/Statistic/UseCases/GetCollectionsRequestHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Squirlish.Data.Repositories;
+using Squirlish.Domain.Collections.Model;
 using Squirlish.Domain.Statistic.Model;
 
 namespace Squirlish.Domain.Statistic.UseCases;
@@ -18,9 +19,29 @@ public class GetCollectionsRequestHandler : IRequestHandler<GetStatisticRequest,
         GetStatisticRequest request,
         CancellationToken cancellationToken = default)
     {
-
         var collections = await _collectionsRepository.GetAllCollections();
 
-        return new List<StatisticItem>() { new StatisticItem { Name = "Слів вивчено", Value = "2" } };
+        return new List<StatisticItem>
+        {
+            new() { Name = "Слів відкрито", Value = $"{GetCountOfOpenedWords(collections)}" },
+            new() { Name = "Слів вивчено", Value = $"{GetCountOfLearnedWords(collections)}" },
+            new() { Name = "Слів частково вивчено", Value = $"{GetCountOfPartiallyLearnedWords(collections)}" }
+        };
+    }
+
+    private static int GetCountOfOpenedWords(ICollection<WordsCollection> collections)
+    {
+        return collections.SelectMany(x => x.Words).Count();
+    }
+
+    private static int GetCountOfLearnedWords(ICollection<WordsCollection> collections)
+    {
+        return collections.SelectMany(x => x.Words)
+            .Count(w => w.Translations.Select(x => x.Language).Distinct().Count() == w.LearningProgress.Count);
+    }
+    private static int GetCountOfPartiallyLearnedWords(ICollection<WordsCollection> collections)
+    {
+        return collections.SelectMany(x => x.Words)
+            .Count(w => w.LearningProgress.Any());
     }
 }
